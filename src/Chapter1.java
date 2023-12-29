657307e0a789a8e51aff94b34f40497f2885b1f2
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-12-30 00:23:42 +0900

    chapter1-3

---
+++ src/Chapter1.java
@@ -0,0 +1,9 @@
+public class Chapter1 {
+    public static void main(String[] args) {
+        boolean result = true;
+        char character = '駆';
+        double pi = 3.14;
+        long number = 3141559265853979L;
+        String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+}
--- src/chapter0.java
+++ src/chapter0.java
@@ -1,4 +1,4 @@
-public class chapter0 {
+public class Chapter0 {
     public static void main(String[] args) {
         System.out.println("菅原拓真です");
         System.out.println("31歳です");
