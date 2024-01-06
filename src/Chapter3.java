d0d45a8341bd5ddf5ed4586e5bc68526f337764b
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-06 23:43:11 +0900

    practice3-2

--- src/Chapter3.java
+++ src/Chapter3.java
@@ -1,11 +1,10 @@
 public class Chapter3 {
     public static void main(String[] args) {
-        boolean tenki = true;
-        if (tenki == true) {
-            System.out.println("洗濯をします");
-            System.out.println("散歩にいきます");
-        } else {
-            System.out.println("映画を見ます");
+        boolean doorClose = true;
+        while (doorClose = true) {
+            // whileで繰り返し
+            System.out.println("ノックをする");
+            System.out.println("1分待つ");
         }
     }
 }
