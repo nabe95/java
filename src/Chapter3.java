602f8644e18c7260eeeca4147a2a2247add246bd
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-07 17:09:47 +0900

    chapter3-5

--- src/Chapter3.java
+++ src/Chapter3.java
@@ -1,12 +1,20 @@
 public class Chapter3 {
     public static void main(String[] args) {
-        boolean tenki = true;
-        if (tenki == true) {
-            System.out.println("洗濯をします");
-            System.out.println("散歩にいきます");
-        } else {
-        System.out.println("映画を見ます");
-        System.out.println("寝ます");
+        System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
+        int selected = new java.util.Scanner(System.in).nextInt();
+        switch (selected) {
+            case 1 -> {
+                System.out.println("「検索します」");
+            }
+            case 2 -> {
+                System.out.println("「登録します」");
+            }
+            case 3 -> {
+                System.out.println("「削除します」");
+            }
+            case 4 -> {
+                System.out.println("「変更します」");
+            }
         }
     }
 }
