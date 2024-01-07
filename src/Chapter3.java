5c4a6184643356f3cc1c4a48e724ddc1816b94fa
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-07 17:50:54 +0900

    chapter3-6

--- src/Chapter3.java
+++ src/Chapter3.java
@@ -1,20 +1,17 @@
 public class Chapter3 {
     public static void main(String[] args) {
-        System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
-        int selected = new java.util.Scanner(System.in).nextInt();
-        switch (selected) {
-            case 1 -> {
-                System.out.println("「検索します」");
-            }
-            case 2 -> {
-                System.out.println("「登録します」");
-            }
-            case 3 -> {
-                System.out.println("「削除します」");
-            }
-            case 4 -> {
-                System.out.println("「変更します」");
+        System.out.print("[数あてゲーム]");
+        int ans = new java.util.Random().nextInt(10);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("0~9の数字を入力してください");
+            int num = new java.util.Scanner(System.in).nextInt();
+            if (ans == num) {
+                System.out.println("アタリ");
+                break;
+            } else {
+                System.out.print("違います");
             }
         }
+        System.out.println("ゲームを終了します");
     }
 }
