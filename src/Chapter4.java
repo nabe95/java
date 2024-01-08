51f4809c43c6f4843ea985a5e0306284b271aac2
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-08 23:57:29 +0900

    chapter4-4

--- src/Chapter4.java
+++ src/Chapter4.java
@@ -1,11 +1,12 @@
 public class Chapter4 {
     public static void main(String[] args) {
-        int[] moneyList = {12902, 8302, 55100};
-        for (int i = 0; i < moneyList.length; i++) {
-            System.out.println(moneyList[i]);
-        }
-        for (int m : moneyList) {
-            System.out.println(m);
+        int[] numbers = {3, 4, 9};
+        System.out.println("1桁の数字を入力してください");
+        int input = new java.util.Scanner(System.in).nextInt();
+        for (int n : numbers) {
+            if (n == input) {
+                System.out.println("アタリ!");
+            }
         }
     }
 }
