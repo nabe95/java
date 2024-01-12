9be65aa9b6e5ac07dab4f1dd3c5ca399c911a426
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-12 22:49:40 +0900

    practice5-4

--- src/Chapter5.java
+++ src/Chapter5.java
@@ -1,13 +1,12 @@
 public class Chapter5 {
-    public static void methodA() {
-        System.out.println("methodA");
-        methodB();
+    public static void main (String[] args) {
+        System.out.println("メソッドを呼び出します");
+        hello("湊");
+        hello("朝香");
+        hello("菅原");
+        System.out.println("メソッドの呼び出しが終わりました");
     }
-    public static void methodB() {
-        System.out.println("methodB");
-    }
-    public static void main(String[] args) {
-        methodA();
-
+    public static void hello(String name) {
+        System.out.println(name + "さん、こんにちは");
     }
 }
