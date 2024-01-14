f6ef1988aa823115306a88f40c42a3e81126c9a5
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-14 23:00:36 +0900

    practice5-11

--- src/Chapter5.java
+++ src/Chapter5.java
@@ -1,19 +1,12 @@
 public class Chapter5 {
-    // １つ目のaddメソッド
     public static int add(int x, int y) {
         return x + y;
     }
-    // 2つ目のaddメソッド
-    public static double add(double x, double y) {
-        return x + y;
-    }
-    // 3つ目のaddメソッド
-    public static String add(String x,String y) {
-        return x + y;
+    public static int add(int x, int y, int z) {
+        return x + y + z;
     }
     public static void main(String[] args) {
-        System.out.println(add(10, 20));
-        System.out.println(add(3.5, 2.7));
-        System.out.println(add("Hello", "World"));
+        System.out.println("10+20=" + add(10, 20));
+        System.out.println("10+20+30=" + add(10, 20, 30));
     }
 }
