cdd85da82b2a6fdb8246ff9e9900c40bd0e1fe2f
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-16 23:47:39 +0900

    practice1-4

--- src/Practice.java
+++ src/Practice.java
@@ -1,8 +1,12 @@
 public class Practice {
     public static void main(String[] args) {
-        int age = 20;
-        System.out.println("私の年齢は" + age);
-        age = 31;
-        System.out.println("・・・いや、本当の年齢は" + age);
+        double pi = 3.14;
+        int pie = 5;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie * pi);
+        System.out.println("パイの半径を倍にします");
+        pi = 10;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie * pi);
     }
 }
