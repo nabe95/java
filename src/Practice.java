14324164dacdf289a54be464e122e64b2de0de84
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-28 00:05:37 +0900

    練習問題

--- src/Practice.java
+++ src/Practice.java
@@ -1,11 +1,16 @@
 public class Practice {
     public static void main(String[] args) {
-        boolean tenki = true;
-        if (tenki == true) {
-            System.out.println("洗濯をします");
-            System.out.println("散歩にいきます");
-        } else {
-            System.out.println("映画を見ます");
+        // 文字型の配列の宣言,3つの要素を持つ配列を作成
+        String[] array = new String[3];
+
+        // 配列の各要素に値を代入
+        array[0] = "{2,4,6}";
+        array[1] = "{3,6,9}";
+        array[2] = "{4,8,12}";
+
+        // 配列の要素を表示
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("要素[" + i + "] = " + array[i]);
         }
     }
 }
