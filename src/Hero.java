124aaf57ba36bce452cb9a96bfc12b15f95d5034
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-02-07 23:26:00 +0900

    chapter8-16

--- src/Hero.java
+++ src/Hero.java
@@ -9,7 +9,7 @@ public class Hero {
 
      public void sit(int sec){
         this.hp += sec;
-        System.out.println("HPが" + sec + "ポイント回復した");
+        System.out.println("HPが" + sec + "秒座った!");
         System.out.println("HPが" + sec + "ポイント回復した");
     }
     public void slip() {
