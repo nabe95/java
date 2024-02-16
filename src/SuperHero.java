3b69bbae39c1a4b2559c5fca530ed8c10e05decc
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-02-16 23:28:30 +0900

    practice10-6

--- src/SuperHero.java
+++ src/SuperHero.java
@@ -1,13 +1,14 @@
 public class SuperHero extends Hero {
     boolean flying;
-
     public void fly() {
         this.flying = true;
-        System.out.println("飛び上がった!");
+        System.out.println("飛び上がった！");
     }
-
-    public void land() {
+    public void land(){
         this.flying = false;
         System.out.println("着地した！");
+    }
+    public void run() {
+        System.out.println(this.name + "は撤退した");
     }
 }
