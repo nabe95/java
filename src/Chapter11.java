3afa9d943c7db2db2b071eb8b2f6780b3d3d36d5
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-02-23 23:16:43 +0900

    practice11-5

---
+++ src/Chapter11.java
@@ -0,0 +1,7 @@
+public class  Chapter11 {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        Matango m = new Matango();
+        h.attack(m);
+  }
+}
--- src/Hero.java
+++ src/Hero.java
@@ -1,21 +1,7 @@
-public class Hero {
-  String name = "ミナト";
-  int hp = 100;
-
-  public Hero() {
-    System.out.println("Heroのコンストラクタが動作");
-  }
-  public void attack(Matango m) {
+public class Hero extends Character {
+  public void atack(Matango m) {
     System.out.println(this.name + "の攻撃！");
-    m.hp -= 5;
-    System.out.println("5ポイントのダメージをあたえた!");
-  }
-  public final void slip() {
-    this.hp -= 5;
-    System.out.println(this.name + "は転んだ!");
-    System.out.println("5のダメージ");
-  }
-  public void run() {
-    System.out.println(this.name + "は逃げ出した！");
+    System.out.println("敵に10ポイントのダメージをあたえた！");
+    m.hp -= 10;
   }
 }
